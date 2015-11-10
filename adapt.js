48f54cfed4b3ac987a69ba799b14faf2132bd721
Phill Grise <Letian Li>
2015-11-10 18:19:22 +0800

    加入分页功能

--- adapt.js
+++ adapt.js
@@ -1,229 +1,198 @@
 function onFile(files) {
-
   var idAlloc = idAllocf();
   for (var i = 0, f; f = files[i]; i++) {
-    console.log(f.name, f.type, f.size);
-
+    //console.log(f.name, f.type, f.size);
     var reader = new FileReader();
     reader.onload = function(e) {
       // get file content
       var text = e.target.result;
       var lines = text.split('\n');
-
       var arr = [];
-      for(line in lines) {
+      for(var line in lines) {
         if (lines[line][0] == '#')
           continue;
         words = lines[line].split(',');
         divid = words[5].split('-');
-        arr.push({
-            cursor: "pointer",
-            x: idAlloc.alloc(words[6]), 
-            label: words[6], 
-            y: [
-              val2pos(parseInt(divid[0])), 
-              val2pos(parseInt(divid[1]))
-            ], 
-            w4: words[4],
-            w7: words[7],
-            //color: class2color[words[7]],  
-            toolTipContent: words[3] + '\n' + words[4] + '\n' + '(' + divid[0] + '-' + divid[1] + ')',
+        arr.push([
+          words[0], //0序号
+          words[1], //1平台类型     
+          words[2], //2平台名称   
+          words[3], //3装备型号
+          words[4], //4装备名称
+          //words[5],
+          idAlloc.alloc(words[6]), //*** 5 ***分配id
+          words[6], //6隶属单位
+          words[7], //7类别属性
 
-
-            //indexLabel: words[4],
-            //indexLabelOrientation: "vertical",
-            //indexLabelMaxWidth: 50,
-            indexLabelWrap: true,
-            indexLabelPlacement: "outside",
-            //indexLabel: "{y[#index]}", 
-            indexLabelFormatter: function ( e ) {
-            	if(this.y[0] > val2pos(glbl.maximum) || this.y[1] < val2pos(glbl.minimum))
-            		return "";
-            	//console.log(e)
-            	if(e.index == 0)
-            		return e.dataPoint.w4;  
-            	else
-	               return "";
-         	}
-
-          });
+          parseFloat(divid[0]), //8下界
+          parseFloat(divid[1]) //9上界
+        ]);
       };
-      
-
-      grp = {}
-      for(var i in arr) {
-      	if(arr[i].w7 in grp)
-     		grp[arr[i].w7].push(arr[i]);
-      	else {
-      		grp[arr[i].w7] = [];
-      		grp[arr[i].w7].push(arr[i]);
-      	}
-      }
-
-      data = []
-      for(var i in grp)
-      	data.push({
-      	showInLegend: true,
-      	legendText: i,
-
-        type: "rangeBar",
-        yValueFormatString: "",
-        //indexLabel: "{y[#label]}",
-        indexLabelFontSize: 12,
-        dataPoints: grp[i]
-      	})
-
-      glbl.arr = data;
-      
-
-
-
-      sum = idAlloc.sum();
-
-      glbl.stripLines = []
-      for(i=0.5; i<sum; i++){
-      	glbl.stripLines.push({
-		    color:"#d8d8f8",
-        value: i,
-        lineDashType: "dot"
-      	})
-      }
-      drawnow1();
+      onDataLoad(arr, idAlloc.sum());
     }
     reader.readAsText(f);
   }
 }
+
 var glbl = {};
-glbl.maximum = Infinity;
-glbl.minimum = -1;
-var dropZone = document.getElementById('drop_zone');
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', handleFileSelect, false); 
-console.log("长春新时代电子科技有限公司，精益求精 永不满足");
-
-//window.onload = function () {
-var drawnow = function (minimum, maximum) {
-	//console.log(glbl.arr);
-/*	var newArr = jQuery.extend(true, {}, glbl.arr);
-	for(var i in newArr) {
-		for(var j in newArr[i]['dataPoints']){
-			if (newArr[i]['dataPoints'][j]['y'][0] > maximum || newArr[i]['dataPoints'][j]['y'][1] < minimum)
-				newArr[i]['dataPoints'][j]['w4'] = '';
-		}
-	}*/
-  var chart = new CanvasJS.Chart("chartContainer",
-  {
-  	//width: w,
-  	//height: 900,
-    //zoomEnabled: true,
-    title: {
-      text: "设备频率划分图"
-    },
-    theme:"theme1",
-    animationEnabled:true,
-    //exportEnabled: true,
-    axisY: {
-    	minimum: val2pos(minimum),
-    	maximum: val2pos(maximum),
-
-      interval: 1,
-      includeZero: false,
-      labelFormatter: function ( e ) {
-        return pos2val(e.value);  
-        //return e.value;
-     }
-    },
-    axisX: {
-      interval: 1,
-      stripLines: glbl.stripLines
-    },
-    toolTip: {
-      content: "{label} </br> 最小: {y[0]}, 最大: {y[1]}"
-    },
-
-    legend:{
-    	horizontalAlign: "right",
-    	verticalAlign: "bottom"
-    },
-    data: glbl.arr
-  });
-
-  chart.render();
+glbl.linesPerPage = 6;
+glbl.stripLines = []
+for(i=0.5; i<=glbl.linesPerPage; i++){
+  glbl.stripLines.push({
+  color:"#d8d8f8",
+  value: i,
+  lineDashType: "dot"
+  })
 }
 
-var drawnow1 = function () {
-	//console.log(glbl.arr);
-/*	var newArr = jQuery.extend(true, {}, glbl.arr);
-	for(var i in newArr) {
-		for(var j in newArr[i]['dataPoints']){
-			if (newArr[i]['dataPoints'][j]['y'][0] > maximum || newArr[i]['dataPoints'][j]['y'][1] < minimum)
-				newArr[i]['dataPoints'][j]['w4'] = '';
-		}
-	}*/
+//产生全局arr
+//产生全局pgs
+function onDataLoad(arr, s) {
+  var pages = parseInt(s/glbl.linesPerPage);
+  for (var i = 0; i <= pages; i++) {
+    $item = $('<li><a href="#" onClick="onDrawTable('+i.toString()+');">'+(i+1).toString()+'</a></li>');
+    $('#pg').append($item)
+  };
+
+
+  glbl.arr = arr;
+  glbl.sum = s;
+  glbl.pgs = {};
+
+  for (var i in glbl.arr) {
+    key = parseInt(glbl.arr[i][5]/glbl.linesPerPage);
+
+    if (glbl.pgs.hasOwnProperty(key)) {
+      glbl.pgs[key].push(glbl.arr[i]);
+    } else {
+      glbl.pgs[key] = [];
+      glbl.pgs[key].push(glbl.arr[i]);
+    };
+  };
+  onDrawTable(0);
+}
+
+//产生全局data
+function onDrawTable(pageNum) {
+  var data = glbl.pgs[pageNum];
+
+  var minimum = Infinity;
+  var maximum = 0;
+  for (var i in data) {
+    l = parseFloat(data[i][8]);
+    h = parseFloat(data[i][9]);
+    if(l < minimum)
+      minimum = l;
+    if(h > maximum)
+      maximum = h;
+  };
+  minimum = pos2val(parseInt(val2pos(minimum)) - 1);
+  maximum = pos2val(parseInt(val2pos(maximum)) + 1);
+  $('#minx').attr('value', minimum);
+  $('#maxx').attr('value', maximum);
+
+  var LocalArr = [];
+  for (var i in data) {
+    LocalArr.push({
+            cursor: "pointer",
+            x: data[i][5], 
+            label: data[i][6], 
+            y: [
+              val2pos(data[i][8]), 
+              val2pos(data[i][9])
+            ], 
+            w4: data[i][4],
+            w7: data[i][7],
+            toolTipContent: data[i][3] + '\n' + data[i][4] + '\n' + '(' + data[i][8] + '-' + data[i][9] + ')',
+
+            indexLabelWrap: true,
+            indexLabelPlacement: "outside",
+            indexLabelFormatter: function ( e ) {
+              /*
+              if(this.y[0] > val2pos(glbl.maximum) || this.y[1] < val2pos(glbl.minimum))
+                return "";
+              if(e.index == 0)
+                return e.dataPoint.w4;  
+              else
+                 return "";
+              */
+              if(e.index == 0)
+                return e.dataPoint.w4;
+              else
+                return '';
+            }
+    });
+  };
+
+  var grp = {}
+  for(var i in LocalArr) {
+    if(LocalArr[i].w7 in grp)
+    grp[LocalArr[i].w7].push(LocalArr[i]);
+    else {
+      grp[LocalArr[i].w7] = [];
+      grp[LocalArr[i].w7].push(LocalArr[i]);
+    }
+  }
+
+  data = []
+  for(var i in grp)
+    data.push({
+    showInLegend: true,
+    legendText: i,
+
+    type: "rangeBar",
+    yValueFormatString: "",
+    //indexLabel: "{y[#label]}",
+    indexLabelFontSize: 12,
+    dataPoints: grp[i]
+  })
+
+  glbl.data = data;
+  drawnow(minimum, maximum);
+}
+
+var drawnow = function (mini, maxi) {
+
   var chart = new CanvasJS.Chart("chartContainer",
   {
-  	//width: w,
-  	//height: 900,
-    //zoomEnabled: true,
     title: {
       text: "设备频率划分图"
     },
     theme:"theme1",
     animationEnabled:true,
-    //exportEnabled: true,
+    exportEnabled: true,
     axisY: {
+    	minimum: val2pos(mini),
+    	maximum: val2pos(maxi),
+
       interval: 1,
       includeZero: false,
       labelFormatter: function ( e ) {
         return pos2val(e.value);  
-        //return e.value;
-     }
-    },
-    axisX: {
-      interval: 1,
-      stripLines: glbl.stripLines
-    },
-    toolTip: {
-      content: "{label} </br> 最小: {y[0]}, 最大: {y[1]}"
-    },
-
-    legend:{
-    	horizontalAlign: "right",
-    	verticalAlign: "bottom"
-    },
-
-/*
-    data: [
-      {
-      	showInLegend: true,
-
-        type: "rangeBar",
-        yValueFormatString: "",
-        //indexLabel: "{y[#label]}",
-        indexLabelFontSize: 12,
-        dataPoints: arr
       }
-    ]
-*/	data: glbl.arr
+    },
+    axisX: {
+      interval: 1,
+      stripLines: glbl.stripLines
+    },
+    toolTip: {
+      content: "{label} </br> 最小: {y[0]}, 最大: {y[1]}"
+    },
+
+    legend:{
+    	horizontalAlign: "right",
+    	verticalAlign: "bottom"
+    },
+    data: glbl.data
   });
+
   chart.render();
 }
-/*
-function chx () {
-	w = $( "#defaultSlider" ).val() ;
-	//console.log(   );
-	drawnow(w);
-}*/
 
 $(document).ready(function(){
 	$("input").change(function(){
 		i = parseFloat($( "#minx" ).val() );
 		a = parseFloat($( "#maxx" ).val() );
-		//console.log(i);
-		//console.log(a);
-		glbl.maximum = a;
-		glbl.minimum = i;
     drawnow(i , a);
 	});
 });
-
